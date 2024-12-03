d1a53c08fe7bfd3738205b226ab00cbd9ff406d9
climatechange-tech <jagabantxo@gmail.com>
2024-12-03 15:59:40 +0100

    Add dictionary with intuitive error messages depending on the exception code raised by a SQLAlchemy operation.

--- config_params.py
+++ config_params.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep 22 12:35:48 2024
-
-@author: jonander
-
 ** DISCLAIMER **
 This program serves as a module to store configuration data
 like credentials, host info, etc.
@@ -17,9 +13,17 @@ like credentials, host info, etc.
 #%% 1. PROGRAMMING CONCEPTS
 
 # Databases
-config_dict = {
+DATABASE_CREDENTIALS = {
     "username": "username",
     "password": "cool-password",
     "host": "host",
     "database_name": "dbname"
+}
+
+data_uploading_error_dict = {
+    "1007": "Database already exists",
+    "1045": "Wrong username",
+    "1049": "Unknown database name",
+    "1698": "Wrong password",
+    "2003": "Wrong host name"
 }
