4fd6eaa9db3052216cf6ae8b5da22cf8c7f8b940
climatechange-tech <jagabantxo@gmail.com>
2024-12-03 16:18:14 +0100

    Add variables to the 'config_params' module
    - Rename `config_dict` to `DATABASE_CREDENTIALS`. This variable will store the credentials for the database connection, and the uppercase name will make it clear that it´s a constant.
    - Add `data_uploading_error_dict` dictionary with intuitive error messages depending on the exception code raised by a SQLAlchemy operation.

--- config_params.py
+++ config_params.py
@@ -17,9 +17,17 @@ like credentials, host info, etc.
 #%% 1. PROGRAMMING CONCEPTS
 
 # Databases
-config_dict = {
+DATABASE_CREDENTIALS = {
     "username": "username",
     "password": "cool-password",
     "host": "host",
     "database_name": "dbname"
+}
+
+data_uploading_error_dict = {
+    "1007": "Database already exists",
+    "1045": "Wrong username",
+    "1049": "Unknown database name",
+    "1698": "Wrong password",
+    "2003": "Wrong host name"
 }
